d6dde9e114ae3a6b80c5d565b9a7b72e4c99f9a4
Navakun Munkid <kunkid28147@gmail.com>
2025-10-30 22:15:56 +0700

    update teacher register

--- api/diag_insert.php
+++ api/diag_insert.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-require_once __DIR__ . '/db.php';
+require_once __DIR__ . '/../config/db.php';
 
 try {
     $pdo->beginTransaction();
